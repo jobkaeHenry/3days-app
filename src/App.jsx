28ba3b9266506db548b4b7893dd28b940525b20e
Jungu Lee <1zzangjun@gmail.com>
2022-11-11 00:27:50 +0900

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Routes } from "react-router";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Navbar from "./Components/Navbar";
+import Profile from "./Pages/Profile";
+import Main from "./Pages/Main";
+import Signup from "./Pages/Signup";
+import Missing from "./Pages/Missing";
+import Login from "./Pages/Login";
+import DevTools from "./Components/DevTools";
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* Navbar는 리랜더링 되지않고, URL에 따라 하위 컴포넌트만 리랜더링 되게하기 위한 구조입니다 */}
+      <DevTools/>
+      <Navbar />
+      {/* 양옆 Padding을 제공하는 Wrapper 입니다 */}
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* 잘못된 경로일때 보내는 곳*/}
+        <Route path="*" element={<Missing />} />
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+
+export default App;
