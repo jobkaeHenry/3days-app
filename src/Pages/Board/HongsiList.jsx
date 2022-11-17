e752ed4281d5928be567c5575bea009d8dfd6a4a
jobkaeHenry <1zzangjun@gmail.com>
2022-11-17 22:01:24 +0900

    asd

--- src/Pages/Board/HongsiList.jsx
+++ src/Pages/Board/HongsiList.jsx
@@ -17,8 +17,8 @@ const HongsiList = () => {
   return !isLoading && data ? (
     <MainContentContainer>
       <MainCenterWrapper>
-      <ColumnCenterWrapper as={"section"} className="mt-36">
-          {data.map((e) => {
+      <ColumnCenterWrapper as={"section"} className={"mt-36 justify-center-mobile"}>
+          {data.map((e,i) => {
             return <HongSiCard props={e} key={e.hongsi_id}/>;
           })}
         </ColumnCenterWrapper>
