36f409131c782d38ca7b5e0c1ec4787e2bd84a3c
Jungu Lee <1zzangjun@gmail.com>
2022-11-19 01:10:18 +0900

    endpoint fixed

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,10 @@
 import styled from "@emotion/styled";
 import { useState, useEffect } from "react";
-import { useForm } from 'react-hook-form';
-import axios from 'axios';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { userState } from "../Recoil/atoms/atom";
-import { Navigate } from "react-router-dom";
-
+import { Link, Navigate } from "react-router-dom";
 
 const Container = styled.div`
   width: 300px;
@@ -77,47 +76,49 @@ const MainContainer = styled.div`
 `;
 
 function Login() {
-  const [error, setErrMsg] = useState('');
+  const [error, setErrMsg] = useState("");
   const setUser = useSetRecoilState(userState);
   const {
     register,
     formState: { errors },
     handleSubmit,
     setFocus,
-  } = useForm({ 
-    mode: "onChange", });
+  } = useForm({
+    mode: "onChange",
+  });
 
-    useEffect(() => {
-      setFocus('email');
-    }, [setFocus]);
+  useEffect(() => {
+    setFocus("email");
+  }, [setFocus]);
 
   const onLogin = async (data) => {
     try {
-    axios.post('/auth/token', {
-      headers: {
-        'Content-Type': 'application/json'
-    },
-      email: data.email,
-      password : data.password,
-
-    }).then((res)=>{
-      if(res.status===200){
-        setUser(res.data)
-        Navigate()
+      axios
+        .post("/auth/token", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          email: data.email,
+          password: data.password,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            setUser(res.data);
+            Navigate();
+          }
+        });
+    } catch (err) {
+      console.log(err);
+      if (!err?.response) {
+        setErrMsg("서버로부터 응답이 없습니다");
+      } else if (err.response?.status === 400) {
+        setErrMsg("이메일 또는 패스워드를 확인해주세요");
+        console.log(error);
+      } else if (err.response?.status === 401) {
+        setErrMsg("허가되지않은 접근입니다");
+      } else {
+        setErrMsg("Login Failed");
       }
-    })
-  } catch (err) {
-    console.log(err);
-    if (!err?.response) {
-      setErrMsg("서버로부터 응답이 없습니다");
-    } else if (err.response?.status === 400) {
-      setErrMsg("이메일 또는 패스워드를 확인해주세요");
-      console.log(error);
-    } else if (err.response?.status === 401) {
-      setErrMsg("허가되지않은 접근입니다");
-    } else {
-      setErrMsg("Login Failed");
-    }
     }
   };
 
@@ -126,44 +127,50 @@ function Login() {
       <Container>
         <Form onSubmit={handleSubmit(onLogin)}>
           <InputContainer>
-            <Label htmlFor={'Email'}>Email</Label>
+            <Label htmlFor={"Email"}>Email</Label>
             <Input
-              type={'email'}
+              type={"email"}
               id="Email"
-              {...register('email', {
+              {...register("email", {
                 required: true,
                 pattern: /^\S+@\S+$/i,
                 maxLength: 50,
               })}
             />
-            {errors.email && errors.email.type === 'required' && (
+            {errors.email && errors.email.type === "required" && (
               <Errormsg> 이메일을 입력해주세요.</Errormsg>
             )}
-            {errors.email && errors.email.type === 'pattern' && (
+            {errors.email && errors.email.type === "pattern" && (
               <Errormsg> 이메일 형식이여야 합니다.</Errormsg>
             )}
-            {errors.email && errors.email.type === 'maxLength' && (
+            {errors.email && errors.email.type === "maxLength" && (
               <Errormsg> 최대 길이는 50자 이하여야 합니다</Errormsg>
             )}
           </InputContainer>
           <InputContainer>
-            <Label htmlFor={'password'}>Password</Label>
+            <Label htmlFor={"password"}>Password</Label>
             <Input
-              type={'password'}
+              type={"password"}
               id="password"
-              {...register('password', {
+              {...register("password", {
                 required: true,
                 minLength: 8,
               })}
             />
-            {errors.password && errors.password.type === 'required' && (
+            {errors.password && errors.password.type === "required" && (
               <Errormsg> 패스워드를 입력해주세요</Errormsg>
             )}
-            {errors.password && errors.password.type === 'minLength' && (
+            {errors.password && errors.password.type === "minLength" && (
               <Errormsg> 최소 길이는 8자 이상이여야 합니다</Errormsg>
             )}
           </InputContainer>
-          <SubmitBtn type="submit" value={'Login'}></SubmitBtn>
+
+          <SubmitBtn className="cursor" type="submit" value={"Login"}></SubmitBtn>
+          <Link className="mt-16 mb-16" to={"/signup"}>
+            <span className=" font-gray">
+              <span className="font-main bold">회원가입 </span>하고 목표이루자!
+            </span>
+          </Link>
           {error && <Errormsg> {error}</Errormsg>}
         </Form>
       </Container>
