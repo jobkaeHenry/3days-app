2f0a1db8c50417e063c80d8f7655d382a756b848
Jungu Lee <1zzangjun@gmail.com>
2022-11-19 02:26:05 +0900

    login success

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -112,7 +112,7 @@ function Login() {
         .then((res) => {
           if (res.status === 200) {
             console.log(res.data)
-            setUser(res.data);
+            setUser(res.data.data);
             navigate(from, { replace: true });
           }
         });
